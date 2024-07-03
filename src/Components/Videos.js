104f1d16843f3fd9140de4e1a074c2065140dbf4
yash <jewalkarayash@gmail.com>
2024-07-03 11:11:43 +0530

    fixed minor bugs

--- src/Components/Videos.js
+++ src/Components/Videos.js
@@ -1,9 +1,14 @@
 // Videos.js
 import starting from '../assets/starting.mp4'
-
+import mask from '../assets/maskFinal.png'
 export const VIDEOS = [
     {
         id: 0,
+        url: mask,
+        loaded: false
+    },
+    {
+        id: 1,
         url: starting,
         loaded: false
     },
